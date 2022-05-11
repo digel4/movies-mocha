a0ce96db3c6a8808f8bf7cf0c54aa19644227476
Oliver Fox <hello@oliverfox.uk>
2022-05-11 12:56:32 +0100

    add app.listen functionality

--- app.js
+++ app.js
@@ -2,17 +2,17 @@ const express = require ("express");
 const app = express();
 
 
-// router.get("/hello", (req, res) => {
-//     res.send("hello");
-// });
 app.use(express.static('public')); 
+
 app.get('/', (req, res) => {
     res.send(index)
 });
 
 
-app.listen(3000, () => {
- 	console.log("server listening on port 3000");
- });
-
-// app.listen(process.env.PORT, process.env.IP);
+if (process.env.port && process.env.ip) {
+    app.listen(process.env.PORT, process.env.IP);
+} else {
+    app.listen(3000, () => {
+        console.log("server listening on port 3000");
+    });
+}
