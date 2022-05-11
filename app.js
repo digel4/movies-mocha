a92bf326cf2a56b2fd20d2729a4f96f9070fa82e
Oliver Fox <hello@oliverfox.uk>
2022-05-11 13:21:31 +0100

    app listen test 2

--- app.js
+++ app.js
@@ -8,8 +8,16 @@ app.get('/', (req, res) => {
     res.send(index)
 });
 
-app.listen(process.env.PORT, process.env.IP);
-if (process.env) {
+// app.listen(process.env.PORT, process.env.IP);
+// if (process.env) {
+//     app.listen(process.env.PORT, process.env.IP);
+// } else {
+//     app.listen(3000, () => {
+//         console.log("server listening on port 3000");
+//     });
+// }
+
+if (process.env.NODE_ENV === 'production') {
     app.listen(process.env.PORT, process.env.IP);
 } else {
     app.listen(3000, () => {
